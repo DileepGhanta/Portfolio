abf83a15577ca6361c30e0e790e874810bd4b704
DileepGhanta <saisridileepghanta@gmail.com>
2024-07-30 20:31:41 +0530

    files added

--- src/About.jsx
+++ src/About.jsx
@@ -3,7 +3,16 @@ import './About.css'
 function About(){
     return(
         <div className="About">
-            
+            <div className="About-Content">
+                <h1>Know Who <b>I'M</b></h1>
+                <div className="About-Data">
+                    <p>Hi everyone! I'm a front-end developer, an ML enthusiast, and a computer science undergraduate. I am currently pursuing a Bachelor of Technology in Computer Science and Engineering at SRM University-AP, where I have consistently achieved excellent academic results.    
+                    </p>
+                </div>
+            </div>
+            <div className="About-Image">
+                <img src="./src/assets/about.png" alt="" />
+            </div>
         </div>
     );
 
